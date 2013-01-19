d537a86a6fc50cfe6bb3a17d4e94d510f2658a5c
amahboub <amahboub@199f73a2-bf68-43c5-bd4a-cec53ab09e27>
2013-01-19 11:15:22 +0000

    Correcting a mistake in scripts (forgot a start of line in reg exp to avoid matching  comments)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/psiscience/trunk/Psyche/trunk@321 199f73a2-bf68-43c5-bd4a-cec53ab09e27

--- problems/SatlibBench/survey_lran.sh
+++ problems/SatlibBench/survey_lran.sh
@@ -4,8 +4,8 @@ touch lran.txt
 (for file in $(ls f*00.cnf); do
 problemname="$(echo "$file" | cut -d"." -f1)"
 outputname="$(echo "$file" | cut -d"." -f1).out"
-nbvar="$(grep -w 'p cnf' "$file" | cut -d " " -f3)"
-nbclauses="$(grep -w 'p cnf' "$file" | cut -d " " -f4)"
+nbvar="$(grep -w '^p cnf' "$file" | cut -d " " -f3)"
+nbclauses="$(grep -w '^p cnf' "$file" | cut -d " " -f4)"
 successtime="$(grep Total "$outputname")"
 error="$(grep error "$outputname")"
 line="$problemname"" | ""$nbvar"" | ""$nbclauses"" | ""$successtime"" | ""$error"
