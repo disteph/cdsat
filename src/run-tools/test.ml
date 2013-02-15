d16a17ad501f43af655928087bad10526198f38e
disteph <disteph@199f73a2-bf68-43c5-bd4a-cec53ab09e27>
2013-02-15 16:25:08 +0000

    new version of run-tools
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/psiscience/trunk/Psyche/trunk@369 199f73a2-bf68-43c5-bd4a-cec53ab09e27

--- src/run-tools/test.ml
+++ src/run-tools/test.ml
@@ -3,25 +3,30 @@
 open Kernel
 open Plugin
 open Search
-open Io
+open Printf
 
-module Tests (P:Plugin.Type) = struct
+let write_to_file filename s =
+  let chan = open_out filename in 
+    fprintf chan "%s\n" s;  
+    close_out chan
 
-  include P
-  module Src   = ProofSearch(UF)(UFSet)(UASet)(Empty.Build(UASet))
+module Tests (MyTheory:Theory.Type)(P:Plugin.Type with type literals = MyTheory.Atom.t) = struct
+
+  open P
+  module Src   = ProofSearch(MyTheory)(UF)(UFSet)(UASet)
   module Strat = Strategy(Src.FE)
 
-  let orig_seq my_formula = Src.FE.Seq.EntUF(UASet.empty,UFSet.add my_formula UFSet.empty, UFSet.empty, UFSet.empty,Sequents.Pol.empty)
+  let orig_seq my_formula = Src.FE.Seq.EntUF(UASet.empty,UFSet.add my_formula UFSet.empty, UFSet.empty, UFSet.empty,Src.FE.emptypolmap)
     
   let go formula = print_endline("---");
     if !Flags.debug>0 then print_endline("I am now starting: "^if !Flags.printrhs then Src.FE.Form.toString formula else "");
     Strat.solve(Src.machine (orig_seq formula) Strat.initial_data)
       
-  module Gen = Generate(UF)
+  module MyParser = MyTheory.Parser(UF)
 
   let treatfile filename = print_endline("===========================");
     print_endline("Treating file "^filename);
-    go (Gen.generate_cnf(Io.parse_cnf_file (Io.list_from_string (Io.read_from_file filename) [] 0)))
+    go(MyParser.parse filename)
 
   let treatdir a = print_endline("Treating directory "^a);
     let b = Sys.readdir(a) in
