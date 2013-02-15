ee2670f8330d80faaa3256abcddb7ba443caa098
disteph <disteph@199f73a2-bf68-43c5-bd4a-cec53ab09e27>
2013-02-15 20:31:52 +0000

    place DPLL_WL back into main file
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/psiscience/trunk/Psyche/trunk@380 199f73a2-bf68-43c5-bd4a-cec53ab09e27

--- src/main.ml
+++ src/main.ml
@@ -7,124 +7,129 @@ open Formulae
 open Test
 
 include Empty
-module MyPlugin = Naive.MyPlugin.GenPlugin(MyTheory)
+module MyPlugin = DPLL_WL.MyPlugin.GenPlugin(MyTheory)
 
-include Tests(MyTheory)(MyPlugin)
+include Tests(MyTheory)(MyPlugin);;
 
-module MP = MyTheory.Parser(MyPlugin.UF)
-open MP
-include PrintableFormula(MyTheory.Atom)(MyPlugin.UF)
+if (Array.length Sys.argv) > 1 then
+  let a = Sys.argv.(1) in
+    if Sys.is_directory a
+    then let _ = treatdir a in ()
+    else let _ = treatfile a in ()
+				  (* write_to_file "latex/output.tex" (printanswer ss);;*)
+;;  
+
+
+
+(* Basic examples - will only work with plugin Naive
+
+  module MP = MyTheory.Parser(MyPlugin.UF)
+  open MP
+  include PrintableFormula(MyTheory.Atom)(MyPlugin.UF)
 
 (* p(x) \/- !p(x) *)
-let f1 = 
+  let f1 = 
   orN(
-    lit(true,"p",[]),
-    lit(false,"p",[])
+  lit(true,"p",[]),
+  lit(false,"p",[])
   )
 
 (* p(x) \/+ !p(x) *)
-let f2 = 
+  let f2 = 
   orP(
-    lit(true,"p",[]),
-    lit(false,"p",[])
+  lit(true,"p",[]),
+  lit(false,"p",[])
   )
 
 (* !p(x) \/+ p(x) : infinite computation if proof-search is depth-first*)
-let f3 = 
+  let f3 = 
   orP(
-    lit(false,"p",[]),
-    lit(true,"p",[])
+  lit(false,"p",[]),
+  lit(true,"p",[])
   )
 
 (* (a \/- b) \/- (!a /\- !b) *)
 
-let f4 = 
+  let f4 = 
   orN(
-    orN(
-      lit(true,"a",[]),
-      lit(true,"b",[])
-    ),
-    andN(
-      lit(false,"a",[]),
-      lit(false,"b",[])
-    )
+  orN(
+  lit(true,"a",[]),
+  lit(true,"b",[])
+  ),
+  andN(
+  lit(false,"a",[]),
+  lit(false,"b",[])
+  )
   )
 
 (* (a \/+ b) \/- (!a /\- !b) *)
-let f5 = 
+  let f5 = 
   orN(
-    orP(
-      lit(true,"a",[]),
-      lit(true,"b",[])
-    ),
-    andN(
-      lit(false,"a",[]),
-      lit(false,"b",[])
-    )
+  orP(
+  lit(true,"a",[]),
+  lit(true,"b",[])
+  ),
+  andN(
+  lit(false,"a",[]),
+  lit(false,"b",[])
+  )
   )
 
 (* (!a \/+ !b) not provable - naive algorithm goes into infinite computation *)
-let f6 = 
+  let f6 = 
   orP(
-    lit(false,"a", []), 
-    lit(false,"b", [])
+  lit(false,"a", []), 
+  lit(false,"b", [])
   )
 
 (* (!a /\- !b) *)
 
-let f7=
+  let f7=
   andN(
-    lit(false,"a", []), 
-    lit(false,"b", [])
+  lit(false,"a", []), 
+  lit(false,"b", [])
   )
 
-let f8=
+  let f8=
   orN(
-    orP(
-      lit(false,"p",[]),
-      lit(false,"q",[])
-    ),
-    andP(
-      lit(true,"p",[]),
-      lit(true,"q",[])
-    )
+  orP(
+  lit(false,"p",[]),
+  lit(false,"q",[])
+  ),
+  andP(
+  lit(true,"p",[]),
+  lit(true,"q",[])
+  )
   )
 
 (*
-let f9=
+  let f9=
   andN(
-    orP(
-      lit(false,"p",[]),
-      lit(true,"p",[])
-    ),
-    lit(true,"p",[])
+  orP(
+  lit(false,"p",[]),
+  lit(true,"p",[])
+  ),
+  lit(true,"p",[])
   )
 *)
 
 
-let print_test f = "Trying to prove: $"^Src.FE.Form.toString f^"$
+  let print_test f = "Trying to prove: $"^Src.FE.Form.toString f^"$
 
-\\vspace{10pt}\n"^
+  \\vspace{10pt}\n"^
   Src.FE.toString (go f)^"\\vspace{30pt}
 
-";;
+  ";;
 
 
-if Array.length Sys.argv = 1 then
   write_to_file "latex/output.tex" (print_test f1^
-				      print_test f2^
-				      print_test f3^
-				      print_test f4^
-				      print_test f5^
-				      print_test f6^
-				      print_test f7^
-				      print_test f8
-				   )
-else let a = Sys.argv.(1) in
-  if Sys.is_directory a
-  then let _ = treatdir a in ()
-  else let _ = treatfile a in ()
-				(* write_to_file "latex/output.tex" (printanswer ss);;*)
-;;  
+  print_test f2^
+  print_test f3^
+  print_test f4^
+  print_test f5^
+  print_test f6^
+  print_test f7^
+  print_test f8
+  )
 
-
+*)
