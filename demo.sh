bdaa397389f10121a412ee668f5369de22a05b5e
disteph <disteph@199f73a2-bf68-43c5-bd4a-cec53ab09e27>
2013-09-19 13:07:32 +0000

    yui
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/psiscience/trunk/Psyche/trunk@779 199f73a2-bf68-43c5-bd4a-cec53ab09e27

---
+++ demo.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+function pause(){
+   read -p "$*"
+}
+
+function lat(){
+    cd latex
+    pdflatex Main.tex
+    cd ..
+    evince latex/Main.pdf &
+    pause ""
+}
+
+echo "psyche -theory empty -examples -gplugin naive -latex"
+pause ""
+./main.native -gplugin naive -theory empty -examples -latex
+pause ""
+lat
+echo "psyche -theory empty -examples -latex"
+pause ""
+./main.native -theory empty -examples -latex
+pause ""
+lat
+emacs demo/LRA-test.smt2
+echo "psyche -gplugin dpll_wl -latex demo/LRA-test.smt2"
+pause ""
+./main.native -gplugin dpll_wl -latex demo/LRA-test.smt2
+pause ""
+lat
+emacs demo/cc2.smt2
+echo "psyche -latex demo/cc2.smt2"
+pause ""
+./main.native -latex demo/cc2.smt2
+pause ""
+lat
+emacs demo/UNSAT-test.cnf
+echo "psyche -gplugin dpll_wl -latex demo/UNSAT-test.cnf"
+pause ""
+./main.native -gplugin dpll_wl -latex demo/UNSAT-test.cnf
+pause "finished saving"
+cd latex
+pdflatex Main.tex
+cd ..
+pause ""
+emacs latex/output.tex
+pause ""
+emacs demo/hole7.cnf
+echo "psyche -gplugin dpll_wl demo/hole7.cnf"
+pause ""
+./main.native -gplugin dpll_wl demo/hole7.cnf
+pause ""
+emacs demo/dubois27.cnf
+echo "psyche -gplugin dpll_wl demo/dubois27.cnf"
+pause ""
+./main.native -gplugin dpll_wl demo/dubois27.cnf
