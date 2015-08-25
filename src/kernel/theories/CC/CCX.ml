4df6cc390a8aae7f5601e0b52140e1fa1c1b1582
graham-lengrand <lengrand@lix.polytechnique.fr>
2015-08-24 20:24:12 -0700

    simplified CCX a bit

--- src/kernel/theories/CC/CCX.ml
+++ src/kernel/theories/CC/CCX.ml
@@ -36,20 +36,21 @@ struct
     else
       begin
         Dump.msg (Some(fun p->p "Procedure called on goal %a under hypotheses\n%a" Term.print_in_fmt t TSet.print_in_fmt atomN)) None None;
-	let l = TSet.fold (fun t l -> t::l) atomN [] in
-	let l' = List.fold_left 
-          (fun li e ->
+        let lEq,lNEq = TSet.fold
+          (fun e (lEq,lNEq)  ->
 	    let b,at = X.predicate e in
             match X.root at with
-            | Some(Symbols.Eq _)  -> e::li
-            | Some(Symbols.NEq _) -> li
-            | _ -> failwith "CC(X) can only understand = and <> as predicate symbols")
-          []
-          l
+            | Some(Symbols.Eq _)  when b     -> e::lEq,lNEq
+            | Some(Symbols.NEq _) when not b -> e::lEq,lNEq
+            | Some(Symbols.NEq _) when b     -> lEq,e::lNEq
+            | Some(Symbols.Eq _) when not b  -> lEq,e::lNEq
+            | _ -> lEq,lNEq)
+          atomN
+          ([],[])
         in
-	let phi = List.map X.atoI l' in
-        let at = X.itoA(X.atoI t) in
-	let s = X.directSubterms at in
+	let phi = List.map X.atoI lEq in
+        let at  = X.itoA(X.atoI t) in
+	let s   = X.directSubterms at in
         let result = 
           match X.root at with
           | Some(Symbols.Eq _) -> 
@@ -62,26 +63,25 @@ struct
 	        Some(List.fold_left (fun e a -> TSet.add a e) TSet.empty li)
             end
           | Some(Symbols.NEq _) ->
-            begin
-	      let r = ref true and ans = ref [] and li = ref (TSet.fold (fun i l ->
-                let at' = X.itoA(X.atoI i) in
-                match X.root i with
-                | Some(Symbols.Eq _) -> at'::l
-                | _ -> l) atomN []) in
-	      while !r && !li<>[] do
-	        let s' = X.directSubterms(List.hd !li) in
-	        let q = Some(List.map2
+            List.fold_right
+              (fun eq -> function
+              | Some answer as a -> a
+              | None ->
+	        let s' = X.directSubterms eq in
+	        let q  = Some(List.map2
                                (fun a a' -> Alg.toQuery (Eq(a,a')))
                                s s') in
-	        match Alg.solve phi q with
-	        | None   -> li := List.tl !li
-	        | Some l -> r:=false; ans := l
-	      done;
-	      if !r then None else
-	        let lis = List.map X.itoA !ans in
-	        Some(List.fold_left (fun e a -> TSet.add a e) TSet.empty ((List.hd !li)::lis))
-            end
-          | _ -> failwith "CC(X) can only understand = and <> as predicate symbols"
+                begin
+	          match Alg.solve phi q with
+                  | None -> None
+                  | Some ans -> 
+	            let lis = List.map X.itoA ans in
+	            Some(List.fold_left (fun e a -> TSet.add a e) TSet.empty (eq::lis))
+                end
+              )
+              lEq
+              None
+          | _ -> None
         in
         (match result with 
         | None   -> Dump.msg (Some(fun p->p "Procedure finished with CONSISTENT with hypotheses.")) None None
@@ -91,59 +91,37 @@ struct
 
   let consistency atomN = 
     Dump.msg (Some(fun p->p "Procedure called on\n%a" TSet.print_in_fmt atomN)) None None;
-    let l = TSet.fold (fun t l -> t::l) atomN [] in
-    let l' = List.fold_right
-      (fun e li ->
+    let lEq,lNEq = TSet.fold
+      (fun e (lEq,lNEq)  ->
 	let b,at = X.predicate e in
         match X.root at with
-        | Some(Symbols.Eq _)  -> e::li
-        | Some(Symbols.NEq _) -> li
-        | _ -> failwith "CC(X) can only understand = and <> as predicate symbols")
-      l
-      []
+        | Some(Symbols.Eq _)  when b     -> e::lEq,lNEq
+        | Some(Symbols.NEq _) when not b -> e::lEq,lNEq
+        | Some(Symbols.NEq _) when b     -> lEq,e::lNEq
+        | Some(Symbols.Eq _) when not b  -> lEq,e::lNEq
+        | _ -> lEq,lNEq)
+      atomN
+      ([],[])
     in
+    let phi = List.map X.atoI lEq in 
     let result = 
-      if l' = l then 
-        begin
-	  let phi = List.map X.atoI l in
-	  match Alg.solve phi None with
-	  | None -> None
-	  | Some l -> (*Alg.print_inputlist l; print_newline();*)
-	    let li = List.map X.itoA l in
-	    Some(List.fold_left (fun e a -> TSet.add a e) TSet.empty li)
-        end
-      else 
-        begin
-	  let r = ref None in
-	  let phi = List.map X.atoI l' in 
-          begin
-	    match Alg.solve phi None with
-	    | None -> ()
-	    | Some l -> (*Alg.print_inputlist l; print_newline();*)
-	      let li = (List.map X.itoA l) in
-	      r := Some(List.fold_left (fun e a -> TSet.add a e) TSet.empty li)
-	  end;
- 	  if !r <> None then !r else 
+      match Alg.solve phi None with
+      | Some l -> (*Alg.print_inputlist l; print_newline();*)
+        let li = List.map X.itoA l in
+        Some(List.fold_left (fun e a -> TSet.add a e) TSet.empty li)
+      | None ->
+        List.fold_right
+          (fun neq -> function
+          | Some answer as a -> a
+          | None ->
             begin
-	      let b = ref true and li = ref l and ans = ref TSet.empty in
-	      while !b && !li<>[] do
-                begin
-                  match X.root(List.hd !li) with
-                  | Some(Symbols.Eq _)  -> ()
-                  | Some(Symbols.NEq _) -> 
-                    begin
-	              match goal_consistency atomN (Term.bC Symbols.Neg [List.hd !li]) with
-	              | None   -> ()
-	              | Some s -> b:=false ; ans:=TSet.add (List.hd !li) s
-                    end
-                  | _ -> failwith "CC(X) can only understand = and <> as predicate symbols"
-                end;
-                li := List.tl !li;
-	      done;
-	      if !b then None else
-	        Some(TSet.add (List.hd !li) !ans)
-	    end 
-        end
+	      match goal_consistency atomN (Term.bC Symbols.Neg [neq]) with
+	      | None   -> None
+	      | Some s -> Some(TSet.add neq s)
+            end
+          )
+          lNEq
+          None
     in
     (match result with 
     | None   -> Dump.msg (Some(fun p->p "Procedure finished with CONSISTENT with hypotheses.")) None None
