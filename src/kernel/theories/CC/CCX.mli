4305526b9cae41c46bd02ac0fbeffb145e2e24a2
graham-lengrand <lengrand@lix.polytechnique.fr>
2015-10-08 22:45:35 +0200

    added the mli files for kernel part of CC. Useful to understand the interface with its plugin.

---
+++ src/kernel/theories/CC/CCX.mli
@@ -0,0 +1,49 @@
+(*****************************)
+(* The functor X => CC(X)    *)
+(* producing a ground theory *)
+(*****************************)
+
+open Top
+open Interfaces_basic
+open Messages
+open Specs
+
+open Prop
+open Literals
+
+open Interfaces
+
+type sign
+
+module Make
+  (DS: sig 
+    include GTheoryDSType
+    val proj: Term.datatype -> LitF.t
+  end)
+  (X : SolvableTheory with type VtoTSet.v = DS.TSet.t
+                      and  type t = DS.Term.t
+                      and  type v = DS.Term.t)
+  (U : PersistentUnionFind with type e = X.v 
+                           and  type d = DS.Term.t input) 
+  : 
+sig
+
+  module type State =
+    sig
+      type t
+      val treated : DS.TSet.t
+      val add : DS.TSet.t -> t
+      val normalise :
+        DS.Term.t ->
+        (sign, DS.TSet.t, Top.Messages.thStraight) Top.Messages.thsays
+    end
+
+  type state =
+    | UNSAT of (sign, DS.TSet.t, Top.Messages.thProvable) Top.Messages.thsays
+    | SAT of
+        (sign, DS.TSet.t, Top.Messages.thNotProvable) Top.Messages.thsays *
+        (module State with type t = state)
+
+  val init : (module State with type t = state)
+
+end
---
+++ src/kernel/theories/CC/myTheory.mli
@@ -0,0 +1,31 @@
+open Top
+open Specs
+open Messages
+
+open Prop
+open Literals
+
+type sign
+
+module Make(DS: sig 
+  include GTheoryDSType
+  val proj: Term.datatype -> LitF.t
+end) :  sig
+
+  module type State = sig
+    type t
+    val treated : DS.TSet.t
+    val add : DS.TSet.t -> t
+    val normalise : DS.Term.t -> (sign, DS.TSet.t, thStraight) thsays
+  end
+
+  type state =      
+    | UNSAT of
+          (sign, DS.TSet.t, thProvable) thsays
+    | SAT of
+        (sign, DS.TSet.t, thNotProvable) thsays
+      * (module State with type t = state)
+
+  val init : (module State with type t = state)
+
+end
