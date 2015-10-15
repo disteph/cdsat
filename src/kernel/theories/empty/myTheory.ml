91d596f8b5b0093934aa953d8c92491902214dae
graham-lengrand <lengrand@lix.polytechnique.fr>
2015-10-15 12:45:26 +0200

    Had forgotten to add kernel/theories/empty!

---
+++ src/kernel/theories/empty/myTheory.ml
@@ -0,0 +1,46 @@
+open Top
+open Messages
+open Specs
+
+type sign = unit
+
+module Make(DS: GTheoryDSType) = struct
+
+  open DS
+
+  let rec state atomN
+      = (module struct
+
+        type newoutput = (sign,TSet.t) output
+        type tset = TSet.t
+
+        let treated = (fun () -> atomN)
+
+        let add = (function
+          | None -> Output(None, state atomN)
+          | Some newtset ->
+             let atomN = DS.TSet.union newtset atomN in
+             let tmp = TSet.fold
+               (fun l -> function
+               | Some _ as ans -> ans
+               | None ->
+                  let notl = Term.bC Symbols.Neg [l] in
+                  if TSet.mem notl atomN 
+                  then Some(TSet.add l (TSet.add notl TSet.empty))
+                  else None
+               )
+               atomN
+               None in
+             match tmp with
+             | Some tset -> Output(Some(thProvable () tset), fail_state)
+             | None -> Output(Some(thNotProvable () atomN), state atomN)
+        )
+
+        let normalise = (fun _ -> failwith "Not a theory with normaliser")
+
+        let clone = (fun () -> Output(None, state atomN))
+      end : SlotMachine with type newoutput = (sign,TSet.t) output and type tset = TSet.t)
+
+  let init = state TSet.empty
+
+end
