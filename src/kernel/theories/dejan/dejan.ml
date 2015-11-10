19f229f5de6a8b6e694da2252bce855129824524
Nephe <guillaume.hetier@polytechnique.edu>
2015-11-10 19:43:34 +0100

    Fixed some comments and simplified auxiliary functions

--- src/kernel/theories/dejan/dejan.ml
+++ src/kernel/theories/dejan/dejan.ml
@@ -16,12 +16,13 @@ let rec dejeanAlgoRec stack =
     (* recalculate the constraints each time... *)
     let cons = Trail.createConstraints head in
 
-    (* here we have : either an empty list which means OK,
+    (* here we have : either None which means there is no contradiction up to now,
        either two contradicting equations *)
     match  Trail.checkConstraints cons with
     | Some(t1,t2) ->
       (*print_string "Failure in constraints\n";*)
       (* damned, there is a failure, let's go back*)
+      (* learn a new clause throug FourierMotzkin resolution and backtrack*)
       let previous = Equation.getPreviousEqs [t1; t2] in
 
       print_string "The following subsystem is contradictory : ";
@@ -29,59 +30,61 @@ let rec dejeanAlgoRec stack =
 
       fmResolve stack previous
 
+    (* affect one more variable *)
     |None -> variableChoosing stack cons
 
-    (* case when we resolve *)
-    and fmResolve stack eqs =
-      match stack with
-      | [] -> failwith "stack was empty but should not"
-      | [t] ->  (* no going back possible *)
-                raise (Unsat_failure eqs)
-      | a::b::q ->
-                  print_string "The previous equations will be resolved : ";
-                  Equation.print_eqs eqs;
-                  Printf.printf "there are %i states if we want to go back\n" (List.length (b::q));
-                  print_string "All but this variable will be eliminated : ";
-                  print_string (Trail.getLastAssignedVariable a);
-                  print_string "\n";
-                  try (
-                    (* FM resolution should at least return an inequation *)
-                    let neweq = FourierMotzkin.fourierMotzkin
-                          (Trail.getLastAssignedVariable a) eqs in
-                    print_string "FM resolution output is the following : ";
-                    Equation.print neweq;print_string "\n";
-                    (* new equation found by resolution FM  *)
-                    (* we go back and erase the current state, then add our new equation *)
-                    let newstack = (Trail.addEq b neweq)::q in
-                    (* back to constraint computing. There could be another failure*)
-                    dejeanAlgoRec newstack
-                  )
-                  (* FM Failure : impossible to eliminate all but one variable. This means
-                  the inequations are not linearly independent ; moreover, since they
-                  produced a contradiction, we are sure to obtain something like 0 < 0*)
-                  with FM_Failure ->
-                  (
-                    print_string "FM resolution failed. Going back again.";
-                    fmResolve (b::q) (Equation.getPreviousEqs eqs)
-                  )
-    (* case when we choose a variable *)
-    and variableChoosing stack cons =
-      match stack with
-      | [] -> failwith "Stack was empty but should not"
-      | (head::tail) ->
-          match Trail.chooseUnassignedVariable head with
-          (* the model is complete*)
-          | None -> Trail.getCurrentModel head
-          | Some(var) ->
+(* case when we resolve *)
+and fmResolve stack eqs =
+  match stack with
+  | [] -> failwith "stack was empty but should not"
+  | [t] ->  (* no going back possible *)
+            raise (Unsat_failure eqs)
+  | a::b::q ->
+              print_string "The previous equations will be resolved : ";
+              Equation.print_eqs eqs;
+              Printf.printf "there are %i states if we want to go back\n" (List.length (b::q));
+              print_string "All but this variable will be eliminated : ";
+              print_string (Trail.getLastAssignedVariable a);
+              print_string "\n";
+              try (
+                (* FM resolution should at least return an inequation *)
+                let neweq = FourierMotzkin.fourierMotzkin
+                      (Trail.getLastAssignedVariable a) eqs in
+                print_string "FM resolution output is the following : ";
+                Equation.print neweq;print_string "\n";
+                (* new equation found by resolution FM  *)
+                (* we go back and erase the current state, then add our new equation *)
+                let newstack = (Trail.addEq b neweq)::q in
+                (* back to constraint computing. There could be another failure*)
+                dejeanAlgoRec newstack
+              )
+              (* FM Failure : impossible to eliminate all but one variable. This means
+              the inequations are not linearly independent ; moreover, since they
+              produced a contradiction, we are sure to obtain something like 0 < 0*)
+              with FM_Failure ->
+              (
+                print_string "FM resolution failed. Going back again.";
+                fmResolve (b::q) (Equation.getPreviousEqs eqs)
+              )
 
-            (* choose a value according to the constraints *)
-            let v = Trail.chooseValue cons var in
+(* case when we choose a variable *)
+and variableChoosing stack cons =
+  match stack with
+  | [] -> failwith "Stack was empty but should not"
+  | (head::tail) ->
+      match Trail.chooseUnassignedVariable head with
+      (* the model is complete*)
+      | None -> Trail.getCurrentModel head
+      | Some(var) ->
 
-            print_string "Assigning the value : ";print_string (string_of_num v);
-            print_string " to : ";print_string var; print_string "\n";
+        (* choose a value according to the constraints *)
+        let v = Trail.chooseValue cons var in
 
-            let newState = Trail.assignValue head var v in
-            dejeanAlgoRec (newState::head::tail)
+        print_string "Assigning the value : ";print_string (string_of_num v);
+        print_string " to : ";print_string var; print_string "\n";
+
+        let newState = Trail.assignValue head var v in
+        dejeanAlgoRec (newState::head::tail)
 
 
 
--- src/kernel/theories/dejan/fourierMotzkin.ml
+++ src/kernel/theories/dejan/fourierMotzkin.ml
@@ -4,15 +4,15 @@ include Equation
 (* eliminates a variable from a set of inequations.
 Allows to compute the general FM resolution*)
 let eliminate var eqs =
-    (* separate the inequations *)
+    (* separate the inequations depending wether *)
     let rec separate eqs =
     match eqs with
     | [] -> [], [], []
     | (t::q) -> let pos,neg,nul = separate q in
-                if getCoeff t var >/ (Num.num_of_int 0) then
+                if Equation.getCoeff t var >/ (Num.num_of_int 0) then
                     (t::pos, neg, nul)
                 else begin
-                    if getCoeff t var </ (Num.num_of_int 0) then
+                    if Equation.getCoeff t var </ (Num.num_of_int 0) then
                       (pos, t::neg, nul)
                     else (pos, neg, t::nul)
                 end
@@ -22,51 +22,49 @@ let eliminate var eqs =
     print_eqs pos;print_string "neg : ";
     print_eqs neg;print_string "nul : ";
     print_eqs nul;*)
+
     (* create all the new inequations : we look recursively over
     all the inequations where our variable has a positive
     coefficient, and all where it has a negative one. Those two
     lists were given by the function separate *)
     let rec complete_result l1 l2 accu =
-    match l1, l2 with
-    | [], [] -> accu
-    | (t::q), [] -> accu
-    | [], (t::q) -> accu
-    | (t1::q1), l2 ->
-        complete_result q1 l2
-        (List.fold_left (fun acc eq ->
-            (combine (Num.num_of_int 1  // (getCoeff t1 var)) t1
-                     (Num.num_of_int (-1) // (getCoeff eq var)) eq)::acc) accu l2)
+        match l1, l2 with
+        | [], _ | _, [] -> accu
+        | (t1::q1), l2 ->
+            complete_result q1 l2
+            (List.fold_left (fun acc eq ->
+                (combine (Num.num_of_int 1  // (Equation.getCoeff t1 var)) t1
+                         (Num.num_of_int (-1) // (Equation.getCoeff eq var)) eq)::acc) accu l2)
     in
     complete_result pos neg nul
 
 (* remove the trivial inequalities (no variable)*)
-let rec remove_trivial = function
-    | [] -> []
-    | t::q when isTrivial t -> remove_trivial q;
-    | t::q -> t::(remove_trivial q)
+let remove_trivial ll =
+    List.filter (fun eq -> not(Equation.isTrivial eq)) ll
 
 exception FM_Failure
 
 (* a failure is exceptional. It can happen if the elimination
 is not suited. The exception indicates to the main algorithm that something
 was wrong with the inequations. *)
-let rec fourierMotzkinRec var eqs =
-    match remove_trivial eqs with
-    | [] -> raise FM_Failure
-    | [eq] -> eq
-    | (t::q) ->
-      try
-        let vv = getAnotherActiveVar t var in
-        fourierMotzkinRec var (eliminate vv eqs)
-      (* we did not find another active variable. This is a
-      unitary constraint on "var"... return it, then.*)
-      with Not_found -> t
-
 
 let fourierMotzkin var eqs =
+    let rec fourierMotzkinRec var eqs =
+        match remove_trivial eqs with
+        | [] -> raise FM_Failure
+        | [eq] -> eq
+        | (t::q) ->
+          try
+            let vv = Equation.getAnotherActiveVar t var in
+            fourierMotzkinRec var (eliminate vv eqs)
+          (* we did not find another active variable. This is a
+          unitary constraint on "var"... return it, then.*)
+          with Not_found -> t
+    in
+
     let eq = fourierMotzkinRec var eqs in
     (* return the equation obtained, which dependencies are
     all the PREVIOUS of the equations used for FM resolution *)
     (* that way, we enforce the invariant that all "previouses"
     of an equation are members of the previous state *)
-    setDependance eq (Equation.getPreviousEqs eqs)
+        setDependance eq (Equation.getPreviousEqs eqs)
