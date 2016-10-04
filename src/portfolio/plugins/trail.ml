3ba0c366a66d51986e5d1b0344bbb579f6c39549
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2016-10-04 15:22:01 +0200

    cleaned up conflict analysis function.

--- src/portfolio/plugins/trail.ml
+++ src/portfolio/plugins/trail.ml
@@ -29,24 +29,24 @@ The reason it was added to it was either:
 - It was propagated from other formulae already present in the trail
 - It was a decision
 - It was tried (i.e. a decision that is not on a formula of the finite basis, and therefore can't just be switched when realising this decision leads to conflict)
-
-The bool in propagated indicates whether we know the level of this propagation for certain, i.e. we have exhibited a dependency path from the decision of that level.
    *)
                              
   type nature =
+    | Original
     | Propagated of straight WB.t
     | Decided of both WB.t
     | Tried
-    | Original
 
 
-  (* When doing conflict analysis, the current set of formulae in conflict is implemented as a Conflict.t below. In that set we want to retain some general information:
+  (* When doing conflict analysis, the current set of formulae in conflict is implemented in type t below. In that set we want to retain some general information:
 - level: the maximal level (or possibly an upper bound on it)
 - chrono: the timestamp of the latest formula in the conflict
 - term: that formula
 - nature: the nature of this formula, as above
-- is_uip: is (Some true) if this formula is a UIP, (Some false) if it is definitely not, None if we can't tell - because some other formulae declared of the same level have not confirmed that level *)
-                             
+- is_uip: is true iff this formula is a UIP
+- has_guess: is true iff the highest level is that of a guess (formula whose nature is Tried)
+ *)
+
   type max = {
       level : int;
       chrono : int;
@@ -67,10 +67,10 @@ The bool in propagated indicates whether we know the level of this propagation f
         empty_info  = {
           level = -1;
           chrono = -1;
-          term =  (fun()->failwith "Empty TTrack");
-          nature = (fun()->failwith "Empty TTrack");
-          is_uip = (fun()->failwith "Empty TTrack");
-          has_guess = (fun()->failwith "Empty TTrack");
+          term =  (fun()->failwith "Empty Trail");
+          nature = (fun()->failwith "Empty Trail");
+          is_uip = (fun()->failwith "Empty Trail");
+          has_guess = (fun()->failwith "Empty Trail");
         };
 
         leaf_info = (fun x (i,j,v) ->
@@ -104,108 +104,130 @@ The bool in propagated indicates whether we know the level of this propagation f
 
   end
 
+  (* Creating the data-structure for Trails *)
+                       
   include PATMap.Make(DestWInfo)(I)
 
+  (* Extracts from trail the fragment tset that is used in left-hand
+  side of propa message, from which we removed semsplit. I.e. the
+  formulae in semsplit do not end up in the extraction. *)
+                     
   let get_data trail ?(semsplit=TSet.empty) msg =
     let WB.WB(_,Propa(tset,_)) = msg in
-    inter_poly (fun _ () v->v) (TSet.diff tset semsplit) trail
+    inter_poly (fun _ v () ->v) trail (TSet.diff tset semsplit)
 
-  let rec analyse
-            (trail : t)
-            ?(semsplit=TSet.empty)  (* This is where we collect semsplit points *)
-            ?level                  (* Level of latest contributing decision, only present if it is a guess *)
-            (conflict : unsat WB.t) (* The conflict to analyse *)
-            ?(conflictWdata = get_data trail ~semsplit conflict) (* The conflict's data *)
-            learn                   (* A function to which we can pass stuff to learn *)
-          : (unsat WB.t, int * straight WB.t) sum Deferred.t
-    =
+               
+  let analyse trail conflict learn =
+    
+    let rec aux
+              conflict (* The conflict to analyse *)
+              ?level   (* Level of latest contributing decision, only present if it is a guess *)
+              semsplit (* This is where we collect semsplit formulae *)
+              data     (* Data about { conflict formulae minus semsplit formulae } *)
+            : (unsat WB.t, int * straight WB.t) sum Deferred.t
+      =
 
-    Dump.print ["trail",1] (fun p ->
-        p "Analysing Conflict: %a" WB.print_in_fmt conflict);
-
-    let data = info conflictWdata in
-
-    let finalise msg =
-      let next = get_data trail msg in          
-      let second = if is_empty next then None
-                   else Some((info next).term()) in
       Dump.print ["trail",1] (fun p ->
-          p "Conflict level: %i; first term: %a; second level: %i; second term:%a, inferred propagation:\n%a"
-            data.level
-            Term.print_in_fmt (data.term())
-            (info next).level
-            (pp_print_option Term.print_in_fmt) second
-            WB.print_in_fmt msg
-        );
-      learn conflict (data.term()) second >>| fun ()->
-      Case2((info next).level, msg)
-    in
+          p "Analysing Conflict: %a" WB.print_in_fmt conflict);
 
-    match level with
-    | Some level when level > data.level
-      -> (* Presence of level means latest contributing decision, of level level, is a guess.
-            Everything in the conflict that is of level >= level is a semsplit element.
-            It's time to do the split.
-          *)
-       let t1,rest = TSet.next semsplit in
-       let t2,_ = TSet.next rest in
-       learn conflict t1 (Some t2) >>| fun ()->
-       Case2(level-1, WB.curryfy semsplit conflict)
+      (* This is what we do when we finalise an answer:
+       msg is the propagation message we use for the branch we backjump to.
+       *)
+      
+      let finalise msg =
+        (* First computing the formulae we need for the branch we backjump to *)
+        let next = get_data trail msg in
+        (* Second formula participating to conflict *)
+        let second = if is_empty next then None
+                     else Some((info next).term()) in
+        Dump.print ["trail",1] (fun p ->
+            p "Conflict level: %i; first term: %a; second level: %i; second term:%a, inferred propagation:\n%a"
+              data.level
+              Term.print_in_fmt (data.term())
+              (info next).level
+              (pp_print_option Term.print_in_fmt) second
+              WB.print_in_fmt msg
+          );
+        (* We learn the conflict, watching first and second formulae *)
 
-    | _ ->
-       begin match data.nature() with
+        learn conflict (data.term()) second >>| fun ()->
+        (* Now jumping to level of second formula contributing to conflict *)
+        
+        Case2((info next).level, msg)
+      in
 
-       | Tried -> failwith "Latest formula in conflict is a guess"    
-                           
-       | Original     -> return(Case1 conflict)
+      match level with
+      | Some level when level > data.level ->
+         (* Presence of level means latest contributing decision, of
+          level level, is a guess.  Everything in the conflict that is
+          of level >= level is a semsplit element.  It's time to do
+          the split.  We want to learn the conflict as a clause, so we
+          pick 2 formulae to watch. *)
+         
+         let t1,rest = TSet.next semsplit in
+         let t2,_ = TSet.next rest in
+         learn conflict t1 (Some t2) >>| fun ()->
+         (* Now we output the level to backtrack to, and by curryfying the
+          semsplit formulae we create a propagation message for second branch *)
 
-       | Decided msg  -> finalise (WB.both2straight msg conflict)
+         Case2(level-1, WB.curryfy semsplit conflict)
 
-       | Propagated msg ->
+      | _ -> (* Otherwise we analyse the nature of the latest formula
+              contributing to the conflict *)
 
-          let curry =
-            if (TSet.is_empty semsplit) (* If we are not in semsplit *)
-               && (data.is_uip())       (* and data.term() is a UIP *)
-               && data.level > 0        (* and conflict is not of level 0 *)
-            then (* ...we might use that UIP to switch branches *)
-              Some((WB.curryfy (TSet.singleton(data.term())) conflict))
-            else None
-          in
-          begin match curry with
-          | Some(WB.WB(_,Propa(_,Straight tset)) as msg)
-               when not(subset_poly (fun() _ -> true) tset trail)
-                       (* ...but only if the negation of the UIP is new knowledge *)
-            -> finalise msg
-          | _ ->
-             Dump.print ["trail",1] (fun p ->
-                 let level,chrono,_ = find (data.term()) trail in
-                 p "Explaining %a of level %i and chrono %i, using message\n%a"
-                   Term.print_in_fmt (data.term())
-                   level chrono
-                   WB.print_in_fmt msg
-               );
-             (* Proposed new conflict, and its data *)
-             let newconflict = WB.resolve msg conflict in
-             let newconflictWdata = get_data trail ~semsplit newconflict  in
-             if (info newconflictWdata).has_guess()
-             then
-               (* The latest contributing decision is a guess,
+         begin match data.nature() with
+
+         | Tried -> failwith "Latest formula in conflict is a guess: TO BE IMPLEMENTED"
+                             
+         | Original ->
+            (* It's a formula of the original problem, and so is the
+               whole conflict.  We stop. *)
+
+            return(Case1 conflict)
+
+         | Decided msg  ->
+            (* It's a decision created by PropaBoth message msg.  We
+               create the propagation message we use for the backjump
+               branch, and finalise our answer. *)
+
+            finalise (WB.both2straight msg conflict)
+
+         | Propagated msg ->
+
+            let curry =
+              if (TSet.is_empty semsplit) (* If we are not in semsplit *)
+                 && (data.is_uip())       (* and data.term() is a UIP *)
+                 && data.level > 0        (* and conflict is not of level 0 *)
+              then (* ...we might use that UIP to switch branches *)
+                Some((WB.curryfy (TSet.singleton(data.term())) conflict))
+              else None
+            in
+            begin match curry with
+            | Some(WB.WB(_,Propa(_,Straight tset)) as msg)
+                 when not(subset_poly (fun() _ -> true) tset trail)
+                         (* ...but only if the negation of the UIP is new knowledge *)
+              -> finalise msg
+            | _ ->
+               Dump.print ["trail",1] (fun p ->
+                   let level,chrono,_ = find (data.term()) trail in
+                   p "Explaining %a of level %i and chrono %i, using message\n%a"
+                     Term.print_in_fmt (data.term())
+                     level chrono
+                     WB.print_in_fmt msg
+                 );
+               (* Proposed new conflict, and its data *)
+               let newconflict = WB.resolve msg conflict in
+               let newdata = info(get_data trail ~semsplit newconflict) in
+               if newdata.has_guess()
+               then
+                 (* The latest contributing decision is a guess,
                  and is among the nodes we are about to add:
                  we refuse to resolve *)
-               analyse trail
-                 ~semsplit:(TSet.add (data.term()) semsplit)
-                 ~level:data.level
-                 conflict
-                 ~conflictWdata
-                 learn
-             else
-               analyse trail
-                 ~semsplit
-                 ?level
-                 newconflict
-                 ~conflictWdata:newconflictWdata
-                 learn
-          end
-       end
+                 aux conflict ~level:data.level (TSet.add (data.term()) semsplit) newdata
+               else
+                 aux newconflict ?level semsplit newdata
+            end
+         end
+    in aux conflict TSet.empty (info(get_data trail conflict))
       
 end
--- src/portfolio/plugins/trail.mli
+++ src/portfolio/plugins/trail.mli
@@ -20,34 +20,34 @@ module Make(WB : WhiteBoard) : sig
 
   (* type nature indicates the status of each formula accumulated in the trail so far.
 The reason it was added to it was either:
+- It was in the original problem
 - It was propagated from other formulae already present in the trail
 - It was a decision
 - It was tried (i.e. a decision that is not on a formula of the finite basis, and therefore can't just be switched when realising this decision leads to conflict)
-
-The bool in propagated indicates whether we know the level of this propagation for certain, i.e. we have exhibited a dependency path from the decision of that level.
    *)
                              
   type nature =
+    | Original
     | Propagated of straight WB.t
     | Decided of both WB.t
     | Tried
-    | Original
 
   include PATMapType with type keys = Term.t
                       and type values = int*int*nature
+                      (* First int is level, second int is timestamp*)
                       (* and type infos  = D.infos *)
                       (* and type common = I.common *)
                       (* and type branching = I.branching *)
                       and type ('v,'i) param = (Term.t,'v,int,int,'i) Patricia.poly
   (* and type t = (D.keys,int*int*nature,int,int,D.infos) poly *)
-  (* First int is level, second int is timestamp*)
 
-  val analyse : t
-                -> ?semsplit:TSet.t
-                -> ?level:int
-                -> unsat WB.t
-                -> ?conflictWdata:t
+  val analyse : t             (* the trail *)
+                -> unsat WB.t (* the conflict *)
                 -> (unsat WB.t -> Term.t -> Term.t option -> unit Deferred.t)
-                -> (unsat WB.t, int * straight WB.t) sum Deferred.t
+                (* a function to which we can pass stuff to learn *)
+                -> (unsat WB.t,  (* Either a subset of the original formulae are unsat *)
+                    int * straight WB.t) (* Or there is a level to backjump to,
+                                            with a propagation message to use for the backjump branch *)
+                     sum Deferred.t
                            
 end
