eed78416341f96f89391f312907f2c80c8a3d7d3
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2016-10-13 09:40:23 +0200

    Improved comments.

--- src/lib/pluginsTh_tools/twoWatchedLits.ml
+++ src/lib/pluginsTh_tools/twoWatchedLits.ml
@@ -113,11 +113,9 @@ module Make (C : Config) = struct
               in None, addconstraint c' ~oldwatched:watched varlist t
          end
       | Branch(_,_,l,r) -> 
-         begin
-           match aux t l with
-	   | None, t          -> aux t r
-	   | Some _ as ans, t -> ans, {t with todo = Pqueue.push r t.todo}
-         end
+         match aux t l with
+	 | None, t          -> aux t r
+	 | Some _ as ans, t -> ans, {t with todo = Pqueue.push r t.todo}
     in
     aux t cset
 
@@ -136,9 +134,7 @@ module Make (C : Config) = struct
   let addconstraintNflag constr varlist t =
     let t = addconstraint constr varlist t in
     let cset = CSet.singleton constr in
-    { t with
-      todo = Pqueue.push cset t.todo
-    }
+    { t with todo = Pqueue.push cset t.todo }
 
   let fix var t = 
     if VarMap.mem var t.var2cons
--- src/lib/pluginsTh_tools/twoWatchedLits.mli
+++ src/lib/pluginsTh_tools/twoWatchedLits.mli
@@ -1,25 +1,28 @@
 (******************************************************************)
 (* This is a module based on the 2-watched literals technique in
    SAT-solving, which can be generalised and used for an arbitrary
-   notion of "constraint" on variables.
+   notion of "constraint" on variables, and n variables to watch
+   instead of 2.
 
    In SAT-solving, a constraint would be a clause, the variables being
    boolean variables; in linear arithmetic, a constraint would be an
    inequation, and variables would be arithmetic variables.
 
-   The goal is to quickly identify which constraints become "unit
-   constraints", i.e. contraints where only one variable is left
-   indeterminate. Unit constraints usually lead to an action to
-   perform: in SAT-solving, a clause with only one indeterminate
-   literal forces this literal to be true; in arithmetic, an
-   inequation with only one indeterminate variable imposes a lower or
-   upper bound on the values that this variable may accept.
+   If n=2, the goal is to quickly identify which constraints become
+   "unit constraints" or "constant constraints", i.e. contraints where
+   only one variable is left indeterminate, or zero. Unit or constant
+   constraints usually lead to an action to perform: in SAT-solving, a
+   clause with only one indeterminate literal forces this literal to
+   be true; in arithmetic, an inequation with only one indeterminate
+   variable imposes a lower or upper bound on the values that this
+   variable may accept.
 
-   The technique consists in making every constraint with at least 2
-   indeterminate variables "watch" 2 of these. As soon as one of the 2
+   The technique consists in making every constraint with at least n
+   indeterminate variables "watch" n of these. As soon as one of the n
    watched variables becomes determined, the constraint needs to pick
    another indeterminate variable to watch: if it is impossible, it
-   means the constraint has become a unit constraint. *)
+   means the constraint has strictly fewer than n indeterminate
+   variables, and the algorithm reports the situation. *)
 (******************************************************************)
 
 open General
@@ -36,19 +39,20 @@ module type Config = sig
   (* simplify fixed c
 
      simplifies constraint c according to currently fixed variables
-     ATTENTION: the output and the input hould have the same id
-   *)
+     ATTENTION: the output and the input should have the same id *)
+
   val simplify: fixed -> Constraint.t -> Constraint.t
 
-  (* pick_another constraint number previous var
+  (* pick_another fixed constraint number previous var
 
-     looks for (number) new variables that constraint can watch:
+     looks for (number) variables that constraint can watch (not fixed by fixed):
      - previous is the previous list of variables that contraint watched
      (its length might be different from (number), typically for a new 
      constraint, previous will be the empty list).
 
      The output provides (Some varlist), if varlist is the new list of length
      (number) of variable to watch, or None, if (number) could not be reached. *)
+
   val pick_another: fixed
                     -> Constraint.t
                     -> int
@@ -57,7 +61,7 @@ module type Config = sig
 end
 
 (* Given such a configuration module C, Make(C) provides an
-implementation of the 2-watched literals technique: *)
+implementation of the n-watched literals technique: *)
 
 module Make(C : Config) : sig
   open C
