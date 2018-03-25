441bd77e52eb8ae9c830be101f9eb2343b77d5c5
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2018-03-25 15:24:57 +0200

    updated signature for Egraphworker

--- src/portfolio/plugins.mld/mcts_plugin.mld/egraphWorker.mli
+++ src/portfolio/plugins.mld/mcts_plugin.mld/egraphWorker.mli
@@ -5,12 +5,12 @@ open Interfaces
 
 module Make(WB: WhiteBoardExt)
          (EGraph: Theories.Eq.Interfaces.API
-          with type sign = Theories.Eq.MyTheory.sign
-           and type termdata = WB.DS.Term.datatype
-           and type value  = WB.DS.Value.t
-           and type cval   = WB.DS.CValue.t
-           and type assign = WB.DS.Assign.t
-           and type tset   = WB.DS.TSet.t) : sig
+          with type sign := Theories.Eq.MyTheory.sign
+           and type termdata := WB.DS.Term.datatype
+           and type value  := WB.DS.Value.t
+           and type cval   := WB.DS.CValue.t
+           and type assign := WB.DS.Assign.t
+           and type tset   := WB.DS.TSet.t) : sig
   open WB
   val make : egraph ports -> unit Deferred.t
 end
