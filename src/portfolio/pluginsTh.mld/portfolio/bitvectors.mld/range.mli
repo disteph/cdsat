26dc1be51c8362a00b87c6a0f16c1f52408f879f
Quentin Soubeyran <quentin.soubeyran@polytechnique.edu>
2018-12-02 21:32:28 +0100

    Implemented quickXplain on bitvectors to explain empty ranges

--- src/portfolio/pluginsTh.mld/portfolio/bitvectors.mld/range.mli
+++ src/portfolio/pluginsTh.mld/portfolio/bitvectors.mld/range.mli
@@ -9,6 +9,8 @@ val mem : V.t -> 'a t -> bool
 type 'a update =
   | Range of 'a t
   | Singleton of V.t
-  | Empty of 'a list
+  | Empty of ('a * BDD.t) list
 
 val update : Signal.t -> 'a -> 'a t -> 'a update
+
+val find_explanation : 'a update -> ('a * BDD.t) list option
